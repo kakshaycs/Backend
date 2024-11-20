d099277c6ccc52c22dab9851600dc0f5311689cd
Akshay Kumar Singh <kakshay25540@gmail.com>
2024-11-20 12:17:40 +0530

    Initialise the application

---
+++ src/test/kotlin/com/backend/saurian/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.saurian
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
